d60b289faf6fc4f679d480f7c954dc462c5cf8df
Hasan Çelik <hsn.2903@outlook.com>
2023-06-02 11:48:32 +0300

    add about section

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <section className="section-about">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">
+          Exciting tours for adventorous people
+        </h2>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-2">
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            You&apos;re going to fall ill love with nature
+          </h3>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+            ipsum sapiente aspernatur libero repellat quis consequatur ducimus
+            quam nisi exercitationem omnis earum qui.
+          </p>
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            Live adventures like you never have before
+          </h3>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            nulla deserunt voluptatum nam.
+          </p>
+
+          <a href="#" className="btn-text">
+            Learn more &rarr;
+          </a>
+        </div>
+        <div className="col-1-of-2">
+          <div className="composition">
+            <img
+              src="/images/nat-1-large.jpg"
+              alt="Photo 1"
+              className="composition__photo composition__photo--p1"
+            />
+            <img
+              src="/images/nat-2-large.jpg"
+              alt="Photo 2"
+              className="composition__photo composition__photo--p2"
+            />
+            <img
+              src="/images/nat-3-large.jpg"
+              alt="Photo 3"
+              className="composition__photo composition__photo--p3"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
