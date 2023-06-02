bd40f4bbdee299b0bdad92a8603da6f09db3c8ff
Hasan Çelik <hsn.2903@outlook.com>
2023-06-02 11:49:08 +0300

    add sass stylings

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import { Lato } from "next/font/google";
+
+// import "@/styles/globals.css";
+import "@/styles/main.scss";
+
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
